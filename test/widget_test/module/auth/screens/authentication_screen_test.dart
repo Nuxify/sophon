4a2fa781b592990ff34d2c5bc7464c9e8835646c
josephgcedeno <josephgcedeno@gmail.com>
2022-12-06 16:26:28 +0800

    fix: remove unncessary check for loading and add navigator observer for redirection

--- test/widget_test/module/auth/screens/authentication_screen_test.dart
+++ test/widget_test/module/auth/screens/authentication_screen_test.dart
@@ -6,25 +6,41 @@ import 'package:mocktail/mocktail.dart';
 import 'package:sophon/infrastructures/service/cubit/secure_storage_cubit.dart';
 import 'package:sophon/module/auth/interfaces/screens/authentication_screen.dart';
 import 'package:sophon/module/auth/service/cubit/auth_cubit.dart';
+import 'package:sophon/module/home/service/cubit/greeting_cubit.dart';
 import 'package:sophon/test/main_test.dart';
-import 'package:sophon/utils/wallet_status_storage.dart';
+import 'package:sophon/test/observer_tester.dart';
+import 'package:walletconnect_dart/walletconnect_dart.dart';
 
 class MockAuthCubit extends MockCubit<AuthState> implements AuthCubit {}
 
 class MockSecureStorageCubit extends MockCubit implements SecureStorageCubit {}
 
+class MockWalletConnect extends Mock implements WalletConnect {
+  @override
+  bool get connected => true;
+}
+
+class MockGreetingCubit extends MockCubit<GreetingState>
+    implements GreetingCubit {}
+
 void main() {
   late MockAuthCubit mockAuthCubit;
   late MockSecureStorageCubit mockSecureStorageCubit;
+  late MockGreetingCubit mockGreetingCubit;
 
   final List<String> connectWalletOptions = <String>['Login with Metamask'];
 
   setUp(() {
     mockAuthCubit = MockAuthCubit();
     mockSecureStorageCubit = MockSecureStorageCubit();
+    mockGreetingCubit = MockGreetingCubit();
   });
-  Future<void> pumpWidget(WidgetTester tester) async => tester.pumpWidget(
-        universalPumper(MultiBlocProvider(
+  Future<void> pumpWidget(
+    WidgetTester tester, {
+    NavigatorObserver? observer,
+  }) async =>
+      tester.pumpWidget(
+        MultiBlocProvider(
           providers: [
             BlocProvider<AuthCubit>(
               create: (BuildContext context) => mockAuthCubit,
@@ -32,11 +48,15 @@ void main() {
             BlocProvider<SecureStorageCubit>(
               create: (BuildContext context) => mockSecureStorageCubit,
             ),
+            BlocProvider<GreetingCubit>(
+              create: (BuildContext context) => mockGreetingCubit,
+            ),
           ],
-          child: const Scaffold(
-            body: AuthenticationScreen(),
+          child: universalPumper(
+            const AuthenticationScreen(),
+            observer: observer,
           ),
-        )),
+        ),
       );
   void listenStub() {
     when(() => mockAuthCubit.state).thenReturn(const AuthState());
@@ -46,42 +66,37 @@ void main() {
 
   group('Authentication screen.', () {
     testWidgets(
-        'On openning screen, there should be CircularProgressIndicator to check if there was a previous connection store in secure storage.',
+        'If there is a previous connection it should navigate directly to home screen.',
         (WidgetTester tester) async {
-      listenStub();
+      bool isNavigatedToHomeScreen = false;
 
-      await pumpWidget(tester);
+      final TestObserver observer = TestObserver()
+        ..onReplaced = (Route<dynamic>? route, Route<dynamic>? previousRoute) =>
+            isNavigatedToHomeScreen = true;
+      when(() => mockGreetingCubit.state).thenReturn(const GreetingState());
 
-      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      whenListen(
+        mockAuthCubit,
+        Stream.fromIterable([
+          EstablishConnectionSuccess(
+            connector: MockWalletConnect(),
+            session: SessionStatus(
+              chainId: 1,
+              accounts: [],
+            ),
+            uri: '',
+          ),
+        ]),
+        initialState: const AuthState(),
+      );
 
-      await tester.pumpAndSettle();
-    });
+      await pumpWidget(
+        tester,
+        observer: observer,
+      );
+      await tester.pump();
 
-    group('If there is a previous connection', () {
-      const String provider1 = 'metamask';
-      testWidgets(
-          'and the provider is Metamask, trigger function loginWithMetamask directly and verify if it is called once.',
-          (WidgetTester tester) async {
-        bool isTriggerLoginWithMetamask = false;
-
-        when(() => mockAuthCubit.state).thenReturn(const AuthState());
-
-        when(() => mockSecureStorageCubit.read(key: providerKey))
-            .thenAnswer((_) async => provider1);
-
-        when(() => mockSecureStorageCubit.read(key: statusKey))
-            .thenAnswer((_) async => connected);
-
-        when(() => mockAuthCubit.loginWithMetamask())
-            .thenAnswer((_) async => isTriggerLoginWithMetamask = true);
-
-        await pumpWidget(tester);
-        await tester.pump();
-
-        expect(isTriggerLoginWithMetamask, isTrue);
-
-        verify(() => mockAuthCubit.loginWithMetamask()).called(1);
-      });
+      expect(isNavigatedToHomeScreen, isTrue);
     });
 
     testWidgets(
