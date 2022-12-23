7d6fcc59232cb3784b16fe2c877209632f7d669f
josephgcedeno <josephgcedeno@gmail.com>
2022-12-23 13:03:18 +0800

    feat: check if there is an existing app installed else launch app store

--- lib/module/auth/interfaces/screens/authentication_screen.dart
+++ lib/module/auth/interfaces/screens/authentication_screen.dart
@@ -1,6 +1,8 @@
+import 'package:external_app_launcher/external_app_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sophon/configs/themes.dart';
+import 'package:sophon/internal/wallet_external_configuration.dart';
 import 'package:sophon/module/auth/service/cubit/auth_cubit.dart';
 import 'package:sophon/module/home/interfaces/screens/home_screen.dart';
 import 'package:url_launcher/url_launcher_string.dart';
@@ -13,6 +15,28 @@ class AuthenticationScreen extends StatefulWidget {
 }
 
 class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  
+  Future<void> _launchApp() async {
+    final bool isInstalled = await LaunchApp.isAppInstalled(
+      androidPackageName: metaMaskPackageName,
+      iosUrlScheme: metamaskWalletScheme,
+    );
+
+    /// If there is an exisitng app, just launch the app.
+    if (isInstalled) {
+      if (!mounted) return;
+      context.read<AuthCubit>().loginWithMetamask();
+      return;
+    }
+
+    /// If there is no exisitng app, launch app store.
+    await LaunchApp.openApp(
+      androidPackageName: metaMaskPackageName,
+      iosUrlScheme: metamaskWalletScheme,
+      appStoreLink: metamaskAppsStoreLink,
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -94,8 +118,7 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
                           Padding(
                             padding: const EdgeInsets.only(top: 8.0),
                             child: ElevatedButton.icon(
-                              onPressed: () =>
-                                  context.read<AuthCubit>().loginWithMetamask(),
+                              onPressed: () => _launchApp(),
                               icon: Image.asset(
                                 'assets/images/metamask-logo.png',
                                 width: 16,
