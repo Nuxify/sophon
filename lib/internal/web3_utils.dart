7342bf3b8e10f2d03a750b0e2d147aa322f98da8
Gabriel <josesgabriellu@gmail.com>
2023-05-26 18:05:58 +0800

    feat: eip712 and eip191 support

--- lib/internal/web3_utils.dart
+++ lib/internal/web3_utils.dart
@@ -1,3 +1,14 @@
+import 'package:pointycastle/api.dart';
+import 'package:pointycastle/ecc/api.dart';
+import 'package:pointycastle/ecc/curves/secp256k1.dart';
+import 'package:pointycastle/signers/ecdsa_signer.dart';
+import 'package:pointycastle/digests/sha256.dart';
+import 'package:pointycastle/macs/hmac.dart';
+import 'package:convert/convert.dart';
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:eth_sig_util/eth_sig_util.dart';
+
 /// Get block time by [chainId]
 /// Returned time is in milliseconds (ms)
 int getBlockTime(int chainId) {
@@ -33,4 +44,47 @@ String getNetworkName(int chainId) {
 enum LoginType {
   metaMask,
   google,
+}
+
+Uint8List bigIntToBytes(BigInt value, int length) {
+  Uint8List result = Uint8List(length);
+  for (int i = 0; i < length; i++) {
+    result[length - 1 - i] = (value >> (8 * i)).toUnsigned(8).toInt();
+  }
+  return result;
+}
+
+String signStringWithEIP191(String message, String privateKeyHex) {
+  Uint8List messageBytes = Uint8List.fromList(utf8.encode(message));
+  Uint8List privateKeyBytes = Uint8List.fromList(hex.decode(privateKeyHex));
+
+  ECCurve_secp256k1 curve = ECCurve_secp256k1();
+  ECPrivateKey privateKey =
+      ECPrivateKey(BigInt.parse(hex.encode(privateKeyBytes), radix: 16), curve);
+
+  ECDSASigner signer = ECDSASigner(HMac(SHA256Digest(), 64) as Digest?);
+  signer.init(true, PrivateKeyParameter<PrivateKey>(privateKey));
+
+  ECSignature signature = signer.generateSignature(messageBytes) as ECSignature;
+  Uint8List rBytes = bigIntToBytes(signature.r, 32);
+  Uint8List sBytes = bigIntToBytes(signature.s, 32);
+
+  Uint8List signatureBytes = Uint8List(64);
+  signatureBytes.setRange(0, 32, rBytes);
+  signatureBytes.setRange(32, 64, sBytes);
+
+  return hex.encode(signatureBytes);
+}
+
+String signStringWithEIP712(
+  String message,
+  String privateKeyHex,
+) {
+  final List<int> codeUnits = message.codeUnits;
+  final Uint8List messageInUint8 = Uint8List.fromList(codeUnits);
+
+  return EthSigUtil.signMessage(
+    privateKey: privateKeyHex,
+    message: messageInUint8,
+  );
 }
