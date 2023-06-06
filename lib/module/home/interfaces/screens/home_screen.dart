49f6bca509d89669cd48711f08610947382a6454
JosesGabriel <josesgabriellu@gmail.com>
2023-06-06 10:37:13 +0800

    feat: pull to refresh

--- lib/module/home/interfaces/screens/home_screen.dart
+++ lib/module/home/interfaces/screens/home_screen.dart
@@ -55,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-
+    context.read<Web3Cubit>().fetchGreeting();
     if (widget.provider == WalletProvider.metaMask) {
       /// Execute after frame is rendered to get the emit state of InitializeMetaMaskProviderSuccess
       WidgetsBinding.instance.addPostFrameCallback(
@@ -209,146 +209,159 @@ class _HomeScreenState extends State<HomeScreen> {
                       ],
                     ),
                   ),
-                  Column(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: width * 0.07,
-                          vertical: height * 0.03,
-                        ),
-                        margin: EdgeInsets.only(
-                          left: width * 0.03,
-                          right: width * 0.03,
-                          bottom: height * 0.03,
-                        ),
-                        decoration: const BoxDecoration(
-                          color: kPink2,
-                          borderRadius: BorderRadius.vertical(
-                            bottom: Radius.circular(10),
-                            top: Radius.circular(10),
-                          ),
-                          boxShadow: <BoxShadow>[
-                            BoxShadow(
-                              color: Colors.black12,
-                              spreadRadius: 4,
-                              blurRadius: 8,
+                  Expanded(
+                    child: RefreshIndicator(
+                      onRefresh: () {
+                        return Future<void>.delayed(
+                          const Duration(seconds: 1),
+                          () => context.read<Web3Cubit>().fetchGreeting(),
+                        );
+                      },
+                      child: ListView(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: width * 0.07,
+                              vertical: height * 0.03,
                             ),
-                          ],
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white.withAlpha(60),
-                                borderRadius: BorderRadius.circular(30),
+                            margin: EdgeInsets.only(
+                              left: width * 0.03,
+                              right: width * 0.03,
+                              bottom: height * 0.03,
+                              top: height * 0.2,
+                            ),
+                            decoration: const BoxDecoration(
+                              color: kPink2,
+                              borderRadius: BorderRadius.vertical(
+                                bottom: Radius.circular(10),
+                                top: Radius.circular(10),
                               ),
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 10,
-                                horizontal: 20,
-                              ),
-                              width: width,
-                              child: BlocBuilder<Web3Cubit, Web3State>(
-                                buildWhen:
-                                    (Web3State previous, Web3State current) =>
+                              boxShadow: <BoxShadow>[
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  spreadRadius: 4,
+                                  blurRadius: 8,
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white.withAlpha(60),
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 10,
+                                    horizontal: 20,
+                                  ),
+                                  width: width,
+                                  child: BlocBuilder<Web3Cubit, Web3State>(
+                                    buildWhen: (Web3State previous,
+                                            Web3State current) =>
                                         current is FetchGreetingSuccess ||
                                         current is UpdateGreetingLoading,
-                                builder:
-                                    (BuildContext context, Web3State state) {
-                                  if (state is FetchGreetingSuccess) {
-                                    return Text(
-                                      '"${state.message}"',
-                                      style:
-                                          theme.textTheme.headline6?.copyWith(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w400,
-                                        fontStyle: FontStyle.italic,
-                                      ),
-                                    );
-                                  }
-                                  return LinearProgressIndicator(
-                                    backgroundColor: Colors.transparent,
-                                    color: Colors.white.withOpacity(0.5),
-                                  );
-                                },
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: width * 0.07,
-                          vertical: height * 0.03,
-                        ),
-                        margin: EdgeInsets.symmetric(horizontal: width * 0.03),
-                        decoration: const BoxDecoration(
-                          color: kPink2,
-                          borderRadius: BorderRadius.vertical(
-                            bottom: Radius.circular(10),
-                            top: Radius.circular(10),
-                          ),
-                          boxShadow: <BoxShadow>[
-                            BoxShadow(
-                              color: Colors.black12,
-                              spreadRadius: 4,
-                              blurRadius: 8,
-                            ),
-                          ],
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            TextField(
-                              controller: greetingTextController,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide:
-                                      const BorderSide(color: Colors.white),
+                                    builder: (BuildContext context,
+                                        Web3State state) {
+                                      if (state is FetchGreetingSuccess) {
+                                        return Text(
+                                          '"${state.message}"',
+                                          style: theme.textTheme.headline6
+                                              ?.copyWith(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w400,
+                                            fontStyle: FontStyle.italic,
+                                          ),
+                                        );
+                                      }
+                                      return LinearProgressIndicator(
+                                        backgroundColor: Colors.transparent,
+                                        color: Colors.white.withOpacity(0.5),
+                                      );
+                                    },
+                                  ),
                                 ),
-                                hintText: 'What\'s in your head?',
-                                fillColor: Colors.white.withAlpha(60),
-                                filled: true,
-                              ),
+                              ],
                             ),
-                            SizedBox(
-                              width: width,
-                              child: BlocBuilder<Web3Cubit, Web3State>(
-                                buildWhen:
-                                    (Web3State previous, Web3State current) =>
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: width * 0.07,
+                              vertical: height * 0.03,
+                            ),
+                            margin:
+                                EdgeInsets.symmetric(horizontal: width * 0.03),
+                            decoration: const BoxDecoration(
+                              color: kPink2,
+                              borderRadius: BorderRadius.vertical(
+                                bottom: Radius.circular(10),
+                                top: Radius.circular(10),
+                              ),
+                              boxShadow: <BoxShadow>[
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  spreadRadius: 4,
+                                  blurRadius: 8,
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                TextField(
+                                  controller: greetingTextController,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                      borderSide:
+                                          const BorderSide(color: Colors.white),
+                                    ),
+                                    hintText: 'What\'s in your head?',
+                                    fillColor: Colors.white.withAlpha(60),
+                                    filled: true,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: width,
+                                  child: BlocBuilder<Web3Cubit, Web3State>(
+                                    buildWhen: (Web3State previous,
+                                            Web3State current) =>
                                         current is UpdateGreetingLoading ||
                                         current is UpdateGreetingSuccess ||
                                         current is UpdateGreetingFailed,
-                                builder:
-                                    (BuildContext context, Web3State state) {
-                                  if (state is UpdateGreetingLoading) {
-                                    return ElevatedButton.icon(
-                                      onPressed: () {},
-                                      style: buttonStyle,
-                                      icon: SizedBox(
-                                        height: height * 0.03,
-                                        width: height * 0.03,
-                                        child: const CircularProgressIndicator(
-                                          strokeWidth: 2,
-                                        ),
-                                      ),
-                                      label: const Text(''),
-                                    );
-                                  }
-                                  return ElevatedButton.icon(
-                                    onPressed: () =>
-                                        updateGreeting(widget.provider),
-                                    icon: const Icon(Icons.edit),
-                                    label: const Text('Update Greeting'),
-                                    style: buttonStyle,
-                                  );
-                                },
-                              ),
+                                    builder: (BuildContext context,
+                                        Web3State state) {
+                                      if (state is UpdateGreetingLoading) {
+                                        return ElevatedButton.icon(
+                                          onPressed: () {},
+                                          style: buttonStyle,
+                                          icon: SizedBox(
+                                            height: height * 0.03,
+                                            width: height * 0.03,
+                                            child:
+                                                const CircularProgressIndicator(
+                                              strokeWidth: 2,
+                                            ),
+                                          ),
+                                          label: const Text(''),
+                                        );
+                                      }
+                                      return ElevatedButton.icon(
+                                        onPressed: () =>
+                                            updateGreeting(widget.provider),
+                                        icon: const Icon(Icons.edit),
+                                        label: const Text('Update Greeting'),
+                                        style: buttonStyle,
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                   Container(
                     padding: const EdgeInsets.symmetric(
